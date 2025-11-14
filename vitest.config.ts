e4e05414ce6edee2c44de73bd1e00cf6bacbdd6d
gugol2 <jsanchezcriado@gmail.com>
2025-11-14 10:25:37 +0100

    add test coverage task

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/**/*.test.{ts,tsx}", "src/**/*.integration.test.{ts,tsx}"],
+      // all: true,
+    },
   },
-})
+});
