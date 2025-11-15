8993295846cc8b8b84e5c1cc41657613c5d332f3
gugol2 <jsanchezcriado@gmail.com>
2025-11-15 13:44:23 +0100

    use only todos for the sake of simplicity

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import { App, serve } from 'react-serve-js'
-import { PostRoutes } from './adapters/http/routes/post.js'
 import { RootRoutes } from './adapters/http/routes/root.js'
-import { UserRoutes } from './adapters/http/routes/user.js'
-import { HealthRoutes } from './adapters/http/routes/health.js'
 import { TodoRoutes } from './adapters/http/routes/todo.js'
 
 function Backend () {
   return (
     <App port={6969} parseBody={true}>
       <RootRoutes />
-      <HealthRoutes />
-      <UserRoutes />
-      <PostRoutes />
       <TodoRoutes />
     </App>
   )
