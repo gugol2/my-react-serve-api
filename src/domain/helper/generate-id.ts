d84ab2d4326cc373ef49e50c92b43e068b4e0460
gugol2 <jsanchezcriado@gmail.com>
2025-11-16 15:29:10 +0100

    [RF] change deprecated substr method in favor of slice

--- src/domain/helper/generate-id.ts
+++ src/domain/helper/generate-id.ts
@@ -2,5 +2,5 @@ import { TodoId } from "../entities/todo.js";
 
 // Helper to generate IDs (in real app, use UUID library)
 export const generateId = (): TodoId => {
-  return `todo_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+  return `todo_${Date.now()}_${Math.random().toString(36).slice(2, 9)}`;
 };
