6322a70f6916ecefbb047ca3e54de99b1e41dd07
gugol2 <jsanchezcriado@gmail.com>
2025-11-16 12:39:23 +0100

    create the post routes in adapters/http/routes folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,17 @@
-// import { App, Route, serve, Response } from 'react-serve-js'
+import { App, Route, serve, Response } from 'react-serve-js'
+import { TodoRoutes } from './adapters/http/routes/todo-routes.js'
 
-import { createApp } from './create-app.js'
-import { Todo, TodoStatus } from './domain/entities/todo.js'
-
-// function Backend () {
-//   return (
-//     <App port={6969} parseBody={true}>
-//       <Route path='/' method='GET'>
-//         {async () => {
-//           return <Response json={{ message: 'Welcome to the API Root!' }} />
-//         }}
-//       </Route>
-//     </App>
-//   )
-// }
-
-// serve(<Backend />)
-
-// Example usage
-const exampleUsage = async () => {
-  const app = createApp()
-  const { controller } = app
-
-  // Create a todo
-  console.log('Creating todo...')
-  const createResponse = await controller.createTodo({
-    params: {},
-    body: {
-      title: 'Learn Hexagonal Architecture',
-      description: 'Study the principles of ports and adapters'
-    }
-  })
-  console.log('Create response:', createResponse)
-
-  // Get all todos
-  console.log('\nGetting all todos...')
-  const getAllResponse = await controller.getAllTodos()
-  console.log('Get all response:', getAllResponse)
-
-  // Get todo by ID
-  if (createResponse.status === 201) {
-    const todoId = (createResponse.body as Todo).id
-
-    console.log('\nGetting todo by ID...')
-    const getByIdResponse = await controller.getTodoById({
-      params: { id: todoId },
-      body: {}
-    })
-    console.log('Get by ID response:', getByIdResponse)
-
-    // Update todo
-    console.log('\nUpdating todo...')
-    const updateResponse = await controller.updateTodo({
-      params: { id: todoId },
-      body: {
-        status: 'completed' as TodoStatus
-      }
-    })
-    console.log('Update response:', updateResponse)
-
-    // Delete todo
-    console.log('\nDeleting todo...')
-    const deleteResponse = await controller.deleteTodo({
-      params: { id: todoId },
-      body: {}
-    })
-    console.log('Delete response:', deleteResponse)
-  }
+function Backend() {
+  return (
+    <App port={6969} parseBody={true}>
+      <Route path='/' method='GET'>
+        {async () => {
+          return <Response json={{ message: 'Welcome to the API!' }} />
+        }}
+      </Route>
+      <TodoRoutes />
+    </App>
+  )
 }
 
-// Uncomment to run the example
-exampleUsage()
+serve(<Backend />)
